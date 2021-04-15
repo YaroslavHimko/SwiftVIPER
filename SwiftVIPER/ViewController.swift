f1058cab23bdba13e54a6dd0ed8f92c6fa21c4e1
Yaroslav Himko <yaroslav.himko@gmail.com>
2021-04-15 21:02:16 +0300

    Initial commit

---
+++ SwiftVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftVIPER
+//
+//  Created by Yaroslav Himko on 15.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
